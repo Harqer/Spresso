c2fdb8663e19a51f8e9dd1af69f06f98ec7f0ea5
Harqer <aoolllathens@outlook.com>
2026-07-08 00:04:06 -0400

    Cleanup: Purged legacy Apps SDK MCP folder

--- Retail-Agentic-Commerce/src/apps_sdk/web/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { LoyaltyHeader } from "./LoyaltyHeader";
-export { RecommendationCarousel } from "./RecommendationCarousel";
-export { ShoppingCart } from "./ShoppingCart";
-export { CheckoutButton } from "./CheckoutButton";
-export { CheckoutPage } from "./CheckoutPage";
-export { PaymentSheet } from "./PaymentSheet";
-export { ThemeToggle } from "./ThemeToggle";
