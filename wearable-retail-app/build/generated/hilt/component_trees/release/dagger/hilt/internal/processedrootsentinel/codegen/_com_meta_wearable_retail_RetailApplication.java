b1e9009eb95d73c777c037a79bbc98ab28362f53
Harqer <aoolllathens@outlook.com>
2026-07-16 02:09:40 -0400

    feat: implement local caching, dependency pinning, and Meta SDK v0.8 hardening

--- wearable-retail-app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_meta_wearable_retail_RetailApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.meta.wearable.retail.RetailApplication"
-)
-public final class _com_meta_wearable_retail_RetailApplication {
-}
